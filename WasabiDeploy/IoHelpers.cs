60d42fa62b0d416a21024bc18c89c4239925313b
MDavid <molnardavid84@gmail.com>
2020-02-08 17:42:36 +0100

    Deploy for windows

---
+++ WasabiDeploy/IoHelpers.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WasabiDeploy
+{
+    public static class IoHelpers
+    {
+        public static void DeleteDirectory(string pathToDirectory)
+        {
+            int attemps = 0;
+            if (Directory.Exists(pathToDirectory))
+            {
+                do
+                {
+                    try
+                    {
+                        attemps++;
+
+                        if (attemps > 1)
+                        {
+                            foreach (var f in Directory.GetFiles(pathToDirectory, "*.*", SearchOption.AllDirectories))
+                            {
+                                File.SetAttributes(f, FileAttributes.Normal);
+                                File.Delete(f);
+                            }
+                        }
+
+                        Directory.Delete(pathToDirectory, true);
+                    }
+                    catch (DirectoryNotFoundException)
+                    {
+                        break;
+                    }
+                    catch (Exception ex)
+                    {
+                        if (attemps > 10)
+                        {
+                            throw ex;
+                        }
+                    }
+                }
+                while (Directory.Exists(pathToDirectory));
+            }
+        }
+    }
+}
