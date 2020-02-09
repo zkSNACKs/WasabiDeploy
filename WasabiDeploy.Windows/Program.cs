0176cb2475b01f37e3b637a5a3fcfc17093f35a5
David Molnar <molnardavid84@gmail.com>
2020-02-09 15:45:24 +0100

    Add publish job

--- WasabiDeploy.Windows/Program.cs
+++ WasabiDeploy.Windows/Program.cs
@@ -6,23 +6,9 @@ namespace WasabiDeploy.Windows
 {
     internal class Program
     {
-        private static async Task Main(string[] args)
+        private static void Main(string[] args)
         {
-            var workingDirectory = Path.Combine(Path.GetTempPath(), "wasabi");
-            var publishDirectory = Path.Combine(workingDirectory, "publish");
-            var guiDirectory = Path.Combine(workingDirectory, "WalletWasabi", "WalletWasabi.Gui");
-
-            var versionPrefix = "1.1.10.2";
-
-            IoHelpers.DeleteDirectory(workingDirectory);
-            Directory.CreateDirectory(workingDirectory);
-            await GitTools.CloneAsync("https://github.com/zkSNACKs/WalletWasabi.git", workingDirectory);
-
-            IoHelpers.DeleteDirectory(publishDirectory);
-            await ProcessTools.StartAsync(
-                "dotnet",
-                $"publish --configuration Release --force --output \"{publishDirectory}\" --self-contained true --runtime \"win7-x64\" /p:VersionPrefix={versionPrefix} --disable-parallel --no-cache /p:DebugType=none /p:DebugSymbols=false /p:ErrorReport=none /p:DocumentationFile=\"\" /p:Deterministic=true /p:PublishSingleFile=true",
-                guiDirectory);
+            Console.WriteLine(string.Join("", args));
         }
     }
 }
