6c7cb2ccc08951d122cb7ed95d529eae9f725409
David Molnar <molnardavid84@gmail.com>
2020-02-10 15:15:43 +0100

    add relative dir

--- WasabiDeploy.Publish/Program.cs
+++ WasabiDeploy.Publish/Program.cs
@@ -10,7 +10,7 @@ namespace WasabiDeploy.Publish
     {
         private static async Task Main(string[] args)
         {
-            var rootDirectory = new DirectoryInfo("../../../..").FullName;
+            var rootDirectory = new DirectoryInfo("./../../../..").FullName;
             Console.WriteLine($"Rootdirectory: {rootDirectory}");
             var workingDirectory = Path.Combine(rootDirectory, "WasabiDeploy.Temp");
             var wasabiRepoDirectory = Path.Combine(workingDirectory, "WalletWasabi");
